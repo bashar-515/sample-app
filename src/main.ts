63e6a2c4451b86057c25de749f431ea76a6081d1
Bashar Eid <bashar.eid@viam.com>
2025-04-16 11:35:33 -0400

    checkpoint

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,43 @@
+import * as VIAM from "@viamrobotics/sdk";
+import Cookies from "js-cookie";
 
+const robotNameDivId = "robot-name";
+
+const robotNameDiv: HTMLElement | null = document.getElementById(robotNameDivId);
+
+if (!robotNameDiv) {
+  throw new Error(`Could not find HTML element with ID ${robotNameDivId}`);
+}
+
+let machineId = "";
+
+let apiKeyId = "";
+let apiKeySecret = "";
+
+(async () => {
+  try {
+    machineId = window.location.pathname.split("/")[2];
+
+    ({ id: apiKeyId, key: apiKeySecret } = JSON.parse(Cookies.get(machineId)!));
+    const robot = await (await connect()).appClient.getRobot(machineId);
+
+    robotNameDiv.textContent = robot?.name ?? "Undefined";
+  } catch (error) {
+    console.log(error);
+
+    robotNameDiv.textContent = "Could not retrieve robot. See console for more details";
+  }
+})();
+
+async function connect(): Promise<VIAM.ViamClient> {
+  const opts: VIAM.ViamClientOptions = {
+    serviceHost: "https://pr-8211-appmain-bplesliplq-uc.a.run.app/",
+    credentials: {
+      type: "api-key",
+      authEntity: apiKeyId,
+      payload: apiKeySecret,
+    },
+  };
+
+  return await VIAM.createViamClient(opts);
+}
